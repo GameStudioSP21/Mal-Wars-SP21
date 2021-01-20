8130272168468e2425b7fef58b2c861f51486562
GenieTu <gtu@cogswell.edu>
2021-01-19 17:49:18 -0800

    Add Test Menu

---
+++ Data/Scripts/zMenuScript.asset.pbt
@@ -0,0 +1,8 @@
+﻿Assets {
+  Id: 6349308714497382701
+  Name: "zMenuScript"
+  PlatformAssetType: 3
+  TextAsset {
+  }
+  SerializationVersion: 72
+}
---
+++ Data/Scripts/zMenuScript.lua
@@ -0,0 +1,55 @@
+﻿local button = script:GetCustomProperty("button"):WaitForObject()
+local menu = script.parent
+local toggle = false
+function ToggleMenu()
+	toggle = not toggle
+	menu.isEnabled  = toggle
+	UI.SetCursorVisible(toggle)
+	UI.CanCursorInteractWithUI(toggle)	
+end
+
+function OnClicked(whichButton)
+	--print("button clicked: " .. whichButton.name)
+	ToggleMenu()
+end
+
+function OnHovered(whichButton)
+	--print("button hovered: " .. whichButton.name)
+end
+
+function OnUnhovered(whichButton)
+	--print("button unhovered: " .. whichButton.name)
+end
+
+button.clickedEvent:Connect(OnClicked)
+button.hoveredEvent:Connect(OnHovered)
+button.unhoveredEvent:Connect(OnUnhovered)
+
+function OnBindingPressed(whichPlayer, binding)
+	--print("player " .. whichPlayer.name .. " pressed binding: " .. binding)
+	if (binding == "ability_extra_45") then 
+		ToggleMenu()
+	end
+end
+
+function OnBindingReleased(whichPlayer, binding)
+	--print("player " .. whichPlayer.name .. " released binding: " .. binding)
+	if (binding == "ability_extra_45") then 
+	
+	end
+end
+
+function OnPlayerJoined(player)
+	--print("player joined: " .. player.name)
+	player.bindingPressedEvent:Connect(OnBindingPressed)
+	player.bindingReleasedEvent:Connect(OnBindingReleased)
+	ToggleMenu()
+end
+
+function OnPlayerLeft(player)
+	--print("player left: " .. player.name)
+end
+
+-- on player joined/left functions need to be defined before calling event:Connect()
+Game.playerJoinedEvent:Connect(OnPlayerJoined)
+Game.playerLeftEvent:Connect(OnPlayerLeft)
