cb23609ddaea625209c7bb2e1634aa4b57762498
Asante Azevedo <59629941+Croissantaye@users.noreply.github.com>
2021-02-15 22:33:03 -0800

    CameraController Updated

--- Data/Scripts/AA_CameraController.lua
+++ Data/Scripts/AA_CameraController.lua
@@ -2,15 +2,17 @@ local LOCAL_PLAYER = Game.GetLocalPlayer()
 local activeCamera = LOCAL_PLAYER:GetActiveCamera()
 local camForward = activeCamera:GetTransform():GetForwardVector()
 local camRight = activeCamera:GetTransform():GetRightVector()
+local HOME_POSITION = activeCamera:GetWorldPosition()
 
 -- used for direction of camera movement (W = <1,0,0>, S = <-1,0,0>, A = <0,-1,0>, D = <0,1,0>)
 local direction = Vector3.New()
 -- stores ACCELERATION per frame
-local ACCELERATION = 5
+local ACCELERATION = 3000
 -- used to add velocity each frame (aka acceleration)
 local currVelocity = Vector3.ZERO
 -- used to speed up the camera movement when holding left shift
 local speedMod = 1
+local MAX_SPEED = 100
 
 --bind constants for W, A, S, D
 local W_BIND = "ability_extra_21"
@@ -18,6 +20,7 @@ local A_BIND = "ability_extra_30"
 local S_BIND = "ability_extra_31"
 local D_BIND = "ability_extra_32"
 local SHIFT_BIND = "ability_extra_12"
+local V_BIND = "ability_extra_42"
 
 --Player ref for binding functions
 local PLAYER = Game.GetLocalPlayer()
@@ -26,7 +29,7 @@ local PLAYER = Game.GetLocalPlayer()
 function OnBindingPressed(PLAYER, binding)
     -- holding left shift speeds up camera
     if binding == SHIFT_BIND then
-        speedMod = 1.3
+        speedMod = 2
     elseif binding == W_BIND then
         direction = direction + camForward
     elseif binding == A_BIND then
@@ -52,6 +55,9 @@ function OnBindingReleased(PLAYER, binding)
         direction = direction + camForward
     elseif binding == D_BIND then
         direction = direction - camRight
+    -- reset to home
+    elseif binding == V_BIND then
+        activeCamera:SetWorldPosition(HOME_POSITION)
     end
 end
 
@@ -62,12 +68,25 @@ function Tick(dt)
     -- add (ACCELERATION * direction) to currVelocity to get new currVelocity
     --( .0.6 * speedMod) used to increase camera speed
     -- print(speedMod)
-    currVelocity = (currVelocity + (ACCELERATION * direction)) * (0.8*speedMod)
+    -- currVelocity = (currVelocity + (ACCELERATION * direction)) * (0.8*speedMod)
+    -- print(dt)
+    if currVelocity.sizeSquared < (MAX_SPEED * MAX_SPEED) and currVelocity.sizeSquared >= 0 then
+        currVelocity = (currVelocity + (direction * ACCELERATION * speedMod)) * dt
+        -- print("increase speed")
+    else
+        -- print("no speed increase")
+    end
     -- add velocity to the current camera position
     activeCamera:SetWorldPosition(currCameraPos + currVelocity)
     -- if direction is 0, the the currVelocity for next frame is 0 to stop camera
     if direction == Vector3.ZERO then
-        currVelocity = Vector3.ZERO
+        if currVelocity.sizeSquared > 0 then
+            currVelocity = (currVelocity - (currVelocity * ACCELERATION)) * dt
+            -- print("slowing down")
+        elseif currVelocity.sizeSquared < 0 then
+            currVelocity = Vector3.ZERO
+        end
+        -- currVelocity = Vector3.ZERO
     end
 end
 
--- Data/Scripts/GS_Platform.lua
+++ Data/Scripts/GS_Platform.lua
@@ -1,3 +1,6 @@
+
+local LOCAL_PLAYER = Game.GetLocalPlayer()
+
 Platform = {}
 Platform._index = Platform
 
@@ -7,25 +10,26 @@ function Platform.New(anchors)
 
     self.freeAnchors = anchors
     self.occupiedAnchors = {}
+    self.platformController = PLATFORM_CONTROLLER
 
     --self._Init(Platform)
 
     return self
 end
 
-function Platform:GetClosestAnchor()
-    local allAnchors = freeAnchors
-    for i, anchor in pairs(occupiedAnchors) do
-        allAnchors.insert(i)
-    end
-    local closest = allAnchors[1]
-    for _,anchor in pairs(allAnchors) do
-        if( (object:GetWorldPosition() - self:GetWorldPosition()) < (closest:GetWorldPosition() - self:GetWorldPosition()) ) then
-            closest = object
-        end
-    end
-    return closest
-end
+-- function Platform:GetClosestAnchor()
+--     local allAnchors = freeAnchors
+--     for i, anchor in pairs(occupiedAnchors) do
+--         allAnchors.insert(i)
+--     end
+--     local closest = allAnchors[1]
+--     for _,anchor in pairs(allAnchors) do
+--         if( (object:GetWorldPosition() - self:GetWorldPosition()) < (closest:GetWorldPosition() - self:GetWorldPosition()) ) then
+--             closest = object
+--         end
+--     end
+--     return closest
+-- end
 
 --move anchor from freeAnchors to occupiedAnchors
 function Platform:SetAnchorOccupied(anchor)
@@ -40,10 +44,24 @@ function Platform:SetAnchorFree(anchor)
 end
 
 function Platform:GetClosestFreeAnchor()
-    local closest = freeAnchors[1]
-    for _,anchor in pairs(freeAnchors) do
-        if( (object:GetWorldPosition() - self:GetWorldPosition()) < (closest:GetWorldPosition() - self:GetWorldPosition()) ) then
-            closest = object
+    local cameraPos = LOCAL_PLAYER:GetViewWorldPosition()
+    local cameraTransform = Transform.New(LOCAL_PLAYER:GetViewWorldRotation(), LOCAL_PLAYER:GetViewWorldPosition(), Vector3.New(1,1,1))
+    local lookDirection = cameraTransform:GetForwardVector()
+    local activeCamera = LOCAL_PLAYER:GetActiveCamera()
+    local minInteractDistance = 0
+    if activeCamera then
+        minInteractDistance = activeCamera.currentDistance - 50
+    end
+    
+    local startPos = cameraPos + lookDirection * minInteractDistance
+    local endPos = cameraPos + lookDirection * 10000
+    
+    local hitResult = UI.GetCursorHitResult() or World.Raycast(startPos, endPos, {ignorePlayers = true}) 
+
+    local closest = self.freeAnchors[1]
+    for obj,anchor in pairs(self.freeAnchors) do
+        if( (obj:GetWorldPosition() - hitResult:GetWorldPosition()) < (closest:GetWorldPosition() - hitResult:GetWorldPosition()) ) then
+            closest = obj
         end
     end
     return closest
