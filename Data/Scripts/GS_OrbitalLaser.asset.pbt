b8fce86bdd74d419675d88337df18cf82d7651ac
Asante Azevedo <59629941+Croissantaye@users.noreply.github.com>
2021-02-18 19:51:56 -0800

    adding orbital laser

---
+++ Data/Scripts/GS_OrbitalLaser.asset.pbt
@@ -0,0 +1,8 @@
+Assets {
+  Id: 12554628210385339467
+  Name: "GS_OrbitalLaser"
+  PlatformAssetType: 3
+  TextAsset {
+  }
+  SerializationVersion: 74
+}
---
+++ Data/Scripts/GS_OrbitalLaser.lua
@@ -0,0 +1,9 @@
+local LOCAL_PLAYER = Game:GetLocalPlayer()
+
+local FIRE_BIND = "primary_ability"
+
+LOCAL_PLAYER.bindingPressedEvent:connect(function(_, binding)
+        --check if the player is in the menu or build mode
+        
+    end
+)
