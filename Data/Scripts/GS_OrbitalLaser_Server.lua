2c93fa98173dec16130406b40c86673ef7bc0fc9
Asante Azevedo <59629941+Croissantaye@users.noreply.github.com>
2021-02-27 22:31:21 -0800

    Adding WaveNumber multiplier to laser
    Added a new function to towerDefenders_WaveManager to get the current wave index

--- Data/Scripts/GS_OrbitalLaser_Server.lua
+++ Data/Scripts/GS_OrbitalLaser_Server.lua
@@ -2,27 +2,18 @@ local GAMER_MANAGER = require(script:GetCustomProperty("TowerDefenders_GameManag
 local MODULE = require( script:GetCustomProperty("ModuleManager") )
 local LASER_DAMAGE = script:GetCustomProperty("Damage")
 local RADIUS = script:GetCustomProperty("Radius")
--- local WAVE_MANAGER = require(script:GetCustomProperty("TowerDefenders_WaveManager"))
-local WAVE_VIEW = script:GetCustomProperty("TowerDefenders_WaveView")
-
 
 function COMBAT_WRAP() return MODULE.Get("standardcombo.Combat.Wrap") end
-
-
 
 local fireRadiusSqaured = RADIUS^2
 
 Events.ConnectForPlayer("OLD", function(player, impactPosition)
     local board = GAMER_MANAGER.GetCurrentBoard(player)
 
+    local waveManager = player.serverUserData.activeBoardWaveManager
+    local currWaveNumber = waveManager:GetWaveIndex() + 1
+
     local enemies = board:GetEnemies()
-    local currWave = WAVE_VIEW:GetWaveNumber()
-    -- if(currWave) then
-    --     currWave:GetName()
-    -- else
-    --     print("current wave is nil")
-    -- end
-    print(currWave)
     for i, enemy in pairs(enemies) do
         if Object.IsValid(enemy) then
             local distanceSqauredFromImpact = (enemy:GetWorldPosition() - impactPosition).sizeSquared
@@ -32,7 +23,8 @@ Events.ConnectForPlayer("OLD", function(player, impactPosition)
                 --Attack Data table keys = {object, damage, source, position, rotation, tags}
                 -- local attackData = {object = enemy, damage = LASER_DAMAGE, source = player, position = impactPosition}
                 -- COMBAT_WRAP().ApplyDamage(attackData)
-                enemy:SetNetworkedCustomProperty("CurrentHealth", (enemyCurrHealth - LASER_DAMAGE))
+                enemy:SetNetworkedCustomProperty("CurrentHealth", (enemyCurrHealth - (LASER_DAMAGE * currWaveNumber)))
+                -- print("Did ", LASER_DAMAGE * currWaveNumber, " damage")
             end
         end
     end
--- Data/Tree/Damage_2/AssetManifest.pbt
+++ Data/Tree/Damage_2/AssetManifest.pbt
@@ -7,12 +7,3 @@ Assets {
     AssetId: "UI_SciFi_Ability_Green_008"
   }
 }
-Assets {
-  Id: 17033141427451298610
-  Name: "Sci-fi Ability Purple 025"
-  PlatformAssetType: 9
-  PrimaryAsset {
-    AssetType: "PlatformBrushAssetRef"
-    AssetId: "UI_SciFi_Ability_Purple_025"
-  }
-}
