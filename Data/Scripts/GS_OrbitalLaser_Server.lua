3bd5f30dcf074828aab0dfead79acc0943b48fd2
Asante Azevedo <59629941+Croissantaye@users.noreply.github.com>
2021-02-27 21:01:21 -0800

    Update GS_OrbitalLaser_Server.lua

--- Data/Scripts/GS_OrbitalLaser_Server.lua
+++ Data/Scripts/GS_OrbitalLaser_Server.lua
@@ -2,6 +2,9 @@ local GAMER_MANAGER = require(script:GetCustomProperty("TowerDefenders_GameManag
 local MODULE = require( script:GetCustomProperty("ModuleManager") )
 local LASER_DAMAGE = script:GetCustomProperty("Damage")
 local RADIUS = script:GetCustomProperty("Radius")
+-- local WAVE_MANAGER = require(script:GetCustomProperty("TowerDefenders_WaveManager"))
+local WAVE_VIEW = script:GetCustomProperty("TowerDefenders_WaveView")
+
 
 function COMBAT_WRAP() return MODULE.Get("standardcombo.Combat.Wrap") end
 
@@ -13,6 +16,13 @@ Events.ConnectForPlayer("OLD", function(player, impactPosition)
     local board = GAMER_MANAGER.GetCurrentBoard(player)
 
     local enemies = board:GetEnemies()
+    local currWave = WAVE_VIEW:GetWaveNumber()
+    -- if(currWave) then
+    --     currWave:GetName()
+    -- else
+    --     print("current wave is nil")
+    -- end
+    print(currWave)
     for i, enemy in pairs(enemies) do
         if Object.IsValid(enemy) then
             local distanceSqauredFromImpact = (enemy:GetWorldPosition() - impactPosition).sizeSquared
