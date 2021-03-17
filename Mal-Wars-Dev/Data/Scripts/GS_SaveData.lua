505bc7a3db8c5ffe0d2ea168e340db5a1187b4a4
Coderz <drakerosecode@gmail.com>
2021-03-16 20:32:39 -0700

    Swapped game manager with currently working one.

--- Mal-Wars-Dev/Data/Scripts/GS_SaveData.lua
+++ Mal-Wars-Dev/Data/Scripts/GS_SaveData.lua
@@ -1,9 +1,12 @@
-local GAME_MANAGER = require(script:GetCustomProperty("TowerDefenders_GameManager"))
+print("LOADING GAME MANAGER")
+
 -- local WAVE_MANAGER = require(script:GetCustomProperty("TowerDefenders_WaveManager"))
 
-print("Save data")
-local towersTable = {name = nil,
-                     position = nil}
+print("GAME MANAGER LOADED")
+local towersTable = { 
+    --{name = nil, position = nil},
+} 
+
 local laserTable = {damage = nil,
                     radius = nil }
 
@@ -17,11 +20,16 @@ local saveData = {  towers = towersTable,
 
 Game.playerJoinedEvent:Connect(function (player)
     -- PLAYER = player
+    local GAME_MANAGER = require(script:GetCustomProperty("TowerDefenders_GameManager"))
     local board = GAME_MANAGER.WaitForBoardFromPlayer(player)
     local waveManager = board:GetWaveManager()
     print(waveManager)
 
     waveManager.OnWaveStarted:Connect(function ()
         print("Get save data")
+        local playerData = "ASSUMING PLAYERDATA RETRIEVED"
+        
+        -- Let's say we're saving towers
+        --table.insert(playerData.towers, { name = "Mortar", position = Vector3.New(0,0,0) }
     end)
 end)
---
+++ Mal-Wars-Dev/Data/Scripts/test.asset.pbt
@@ -0,0 +1,8 @@
+Assets {
+  Id: 15712913469023082707
+  Name: "test"
+  PlatformAssetType: 3
+  TextAsset {
+  }
+  SerializationVersion: 78
+}
