5bbbaf03247a77a8468255f0eb2cc123aace2749
GenieTu <gtu@cogswell.edu>
2021-03-31 01:50:52 -0700

    Level Transitions

---
+++ Mal-Wars-Dev/Data/Scripts/LevelTransitionClient.lua
@@ -0,0 +1,20 @@
+-- Level Transitions Client
+-- Move the game from one portal to the next for new level. 
+-- UI for in game when you want to return the level you were just one.
+
+-- Move from portal to portal to go to new level
+-- Add UI menu to return to the hub world whenplayer is done
+
+-- Sends a player to another game.
+-- The game ID can be obtained from the Core website, 
+-- for example to transfer a player to Core Royale, 
+-- we navigate to that game's page at https://www.coregames.com/games/577d80/core-royale 
+-- and copy the last two parts of the URL 577d80/core-royale as the game ID.
+
+local GAME_ID = script:GetCustomProperty("GAME_ID")
+local UIButton = script:GetCustomProperty("UIButton"):WaitForObject()
+
+UIButton.pressedEvent:Connect(function()
+	print("Level Transition Client: " .. GAME_ID)
+	Events.BroadcastToServer("LevelTransition", GAME_ID)
+end)
