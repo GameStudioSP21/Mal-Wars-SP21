eb95306a4ef728f0d9a69b3185d32f7b7d3adc4e
GenieTu <gtu@cogswell.edu>
2021-03-30 23:28:17 -0700

    Remove Game Start & Auto Start Game

--- Mal-Wars-Dev/Data/Scripts/demo_script.lua
+++ Mal-Wars-Dev/Data/Scripts/demo_script.lua
@@ -10,3 +10,21 @@ end)
 Events.Connect("GameStarting",function() 
     UIButton.visibility = Visibility.FORCE_OFF
 end)
+
+
+-- Remove Game Start
+-- Remove the game start check. 
+-- Starts the game automatically 
+-- but player still has to select the 'Wave Start' button first.
+function RemoveGameStart()
+	UIButton.isEnabled = false
+	UIButton.visibility = Visibility.FORCE_OFF
+end
+function AutoStartGame()
+	Task.Wait()
+	Events.BroadcastToServer("StartGame")
+end
+
+RemoveGameStart()
+AutoStartGame()
+
